1d180fbdc73b87af5beca0cd2696f46a9bb13c70
Ha Kieu Minh <minhha19122014@gmail.com>
2020-06-27 17:35:23 +0900

    reassign constructor from Leader's constructor to TechLeader's constructor

--- inheritance.js
+++ inheritance.js
@@ -6,7 +6,13 @@ Leader.prototype.createPlan = function(){
 
 function TechLeader(){}
 
+//Before reassign TechLeader.prototype.constructor = TeachLeader;
+//new TechLeader.prototype.constructor() = new TeachLeader;
 TechLeader.prototype = Object.create(Leader.prototype)
+
+//reassign constructor from Leader's constructor to TechLeader's constructor
+TechLeader.prototype.constructor = TechLeader;
+
 TechLeader.prototype.reviewCode = function(){
     console.log('review code')
 }
